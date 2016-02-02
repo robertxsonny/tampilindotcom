0db5609afc4fe77244add8f02353fbb993557695
rsonnyprakoso <robertusonny@gmail.com>
2016-02-02 13:04:45 +0700

    initial design, masukin foto dan konten (belum selesai)

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/style.css" rel='stylesheet' type='text/css'>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:300,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Raleway:300,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Arimo:700,400' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+<link
+	href='https://fonts.googleapis.com/css?family=Hind+Vadodara:600,400'
+	rel='stylesheet' type='text/css'>
+<title>tampilin.com</title>
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+
+</head>
+<body>
+
+<div id="wrapper">
+	<header id="header">
+			<button type="button" id="button-collapse" class="navbar-toggle" data-toggle="collapse" data-target="#left-navbar">
+       			<i class="fa fa-2x fa-bars"></i>
+      		</button>
+			<h2 id="brand-title">tampilin.com</h2>
+	</header>
+	
+	<div id="content-wrapper">
+		<div id="cover-1" class="page image-page" style="background: linear-gradient(rgba(47,47,59,0.4), rgba(47,47,59,0.4)), url('images/DSC_0074 copy.jpg') no-repeat center fixed; background-size: cover;">
+			<div class="page-content">
+				<span class="title title-center title-1">ONE PAGE WEBSITE</span>
+				<span class="title title-center title-2">for all your needs</span>
+			</div>
+		</div>
+	
+		<div id="overview" class="page image-page" style="background-color: #E61B04; color: #E0E0E0;" >
+		</div>
+	
+		<div id="cover-2" class="page image-page" style="background: linear-gradient(rgba(47,47,59,0.4), rgba(47,47,59,0.4)),  url('images/DSC_0060 copy.jpg') no-repeat center fixed; background-size: cover;">
+			<div class="page-content">
+				<span class="title title-left title-1">PERSONAL</span>
+				<span class="title title-left title-2">or</span>
+				<span class="title title-left title-1">BUSINESS</span>
+			</div>
+		</div>
+	
+		<div id="feature" class="page content-page" style="background-color: #E0E0E0; color: #E61B04;">
+			<div class="page-content">
+				<h1 class="content-title">Sesuaikan dengan kebutuhanmu</h1>
+				<div id="personal" class="box-2">
+					<i class="fa fa-user fa-5x"></i>
+					<p class="desc">Buat website portofolio untuk dirimu sendiri! </p>
+					<h3>Fitur:</h3>
+					<ul class="feature-list">
+						<li>Nama domain bebas (sesuai ketersediaan)</li>
+						<li>Gratis foto (maksimal 5 foto)</li>
+						<li>Dapat menggunakan koleksi foto sendiri (maksimal 10 foto)</li>
+						<li>Gratis konsultasi CV untuk meningkatkan daya tarik website</li>
+					</ul>
+					<h1 class="price">Rp 300.000,00</h1>
+				</div>
+				<span style="display: inline-block; width:1px; height:100%; background: black; height: 30em; margin: 0 2px;"></span>
+				<div id="business" class="box-2">
+					<i class="fa fa-building fa-5x"></i>
+					<p class="desc">Buat website profile sederhana untuk usahamu!</p>
+					<h3>Fitur:</h3>
+					<ul class="feature-list">
+						<li>Nama domain bebas (sesuai ketersediaan)</li>
+						<li>Gratis foto (maksimal 20 foto)</li>
+						<li>Dapat menggunakan koleksi foto sendiri (maksimal 30 foto)</li>
+						<li>Gratis pembuatan deskripsi (jika belum ada)</li>
+					</ul>
+					<h1 class="price">Rp 500.000,00</h1>
+				</div>
+			</div>
+		</div>
+	
+		<div id="cover-3" class="page image-page" style="background: linear-gradient(rgba(47,47,59,0.4), rgba(47,47,59,0.4)),  url('images/DSC_0097 copy.jpg') no-repeat center center fixed; background-size: cover;">
+			<div class="page-content">
+				<span class="title title-right title-1">CREATE</span>
+				<span class="title title-right title-2">your own</span>
+			</div>
+		</div>
+	
+		<div id="how-to" class="page image-page" style="background-color: #E65B5E; color: #2F2F3B;">
+		</div>
+	
+		<div id="about-us" class="page image-page" style="background-color: #2F2F3B; color: #E65B5E;">
+		</div>
+	</div>
+	
+	
+	
+</div>
+
+</body>
+</html>
