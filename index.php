ea95e40ea7e862022e478f44d36162d66e038b91
rsonnyprakoso <robertusonny@gmail.com>
2016-02-03 15:39:47 +0700

    tambah content buat semua halaman

--- index.php
+++ index.php
@@ -26,10 +26,14 @@
 
 <div id="wrapper">
 	<header id="header">
+		<div class="page-content">
 			<button type="button" id="button-collapse" class="navbar-toggle" data-toggle="collapse" data-target="#left-navbar">
        			<i class="fa fa-2x fa-bars"></i>
       		</button>
-			<h2 id="brand-title">tampilin.com</h2>
+			<h2 id="brand-title">
+			<a><img src="images/tampilin_black_red.png" height="40px" /></a>
+			</h2>		
+		</div>
 	</header>
 	
 	<div id="content-wrapper">
@@ -40,7 +44,70 @@
 			</div>
 		</div>
 	
-		<div id="overview" class="page image-page" style="background-color: #E61B04; color: #E0E0E0;" >
+		<div id="overview" class="page content-page" style="background-color: #FF7066; color: #FFFFFF;" >
+			<div class="page-content">
+				<h1 class="content-title">APA YANG KAMI TAWARKAN?</h1>
+				<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-file fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>Sederhana</strong>
+							<br/>
+							<label>Mengembangkan <i>One page website</i> yang modern dan minimalis untukmu</label></p>
+						</div>
+					</div>
+					<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-clock-o fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>Cepat dan Handal</strong>
+							<br/>
+							<label>Kami menjamin websitemu akan siap sebelum matahari terbit esok hari</label></p>
+						</div>
+					</div>
+					<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-user fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>Personal</strong>
+							<br/>
+							<label>Didesain sesuai dengan kebutuhan dan karakteristikmu</label></p>
+						</div>
+					</div>
+					<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-book fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>On Demand</strong>
+							<br/>
+							<label>Tentukan isi websitemu sendiri, portofolio atau profil usaha</label></p>
+						</div>
+					</div>
+					<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-camera fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>Fotografi</strong>
+							<br/>
+							<label>Ambil foto baru untuk website, atau tambahkan fotomu sendiri</label></p>
+						</div>
+					</div>
+					<div class="item-with-icon box-2">
+						<div class="icon-wrapper">
+							<i class="fa fa-cogs fa-3x"></i>
+						</div>
+						<div class="caption-wrapper">
+							<p class="desc desc-left"><strong>Dukungan Penuh</strong>
+							<br/>
+							<label>Gratis <i>maintenance</i> dan perbaikan selama setahun</label></p>
+						</div>
+					</div>
+			</div>
 		</div>
 	
 		<div id="cover-2" class="page image-page" style="background: linear-gradient(rgba(47,47,59,0.4), rgba(47,47,59,0.4)),  url('images/DSC_0060 copy.jpg') no-repeat center fixed; background-size: cover;">
@@ -51,26 +118,27 @@
 			</div>
 		</div>
 	
-		<div id="feature" class="page content-page" style="background-color: #E0E0E0; color: #E61B04;">
+		<div id="feature" class="page content-page" style="background-color: #ECECEC; color: #2F2F3B;">
 			<div class="page-content">
-				<h1 class="content-title">Sesuaikan dengan kebutuhanmu</h1>
-				<div id="personal" class="box-2">
-					<i class="fa fa-user fa-5x"></i>
+				<h1 class="content-title">SESUAIKAN KEBUTUHANMU</h1>
+				<div id="personal" class="box-2 box-border-transparent" style="background-color: #008DC7; color: #ECECEC;">
+					<div class="icon-big" style="background: url('images/image1.png') no-repeat center; background-size: cover;">
+					</div>
 					<p class="desc">Buat website portofolio untuk dirimu sendiri! </p>
-					<h3>Fitur:</h3>
+					<h1 class="content-subtitle">Fitur:</h1>
 					<ul class="feature-list">
 						<li>Nama domain bebas (sesuai ketersediaan)</li>
 						<li>Gratis foto (maksimal 5 foto)</li>
 						<li>Dapat menggunakan koleksi foto sendiri (maksimal 10 foto)</li>
-						<li>Gratis konsultasi CV untuk meningkatkan daya tarik website</li>
+						<li>Gratis konsultasi konten CV/portofolio</li>
 					</ul>
 					<h1 class="price">Rp 300.000,00</h1>
 				</div>
-				<span style="display: inline-block; width:1px; height:100%; background: black; height: 30em; margin: 0 2px;"></span>
-				<div id="business" class="box-2">
-					<i class="fa fa-building fa-5x"></i>
+				<div id="business" class="box-2 box-border-transparent" style="background-color: #59A80F; color: #ECECEC;">
+					<div class="icon-big" style="background: url('images/image2.png') no-repeat center; background-size: cover;">
+					</div>
 					<p class="desc">Buat website profile sederhana untuk usahamu!</p>
-					<h3>Fitur:</h3>
+					<h1 class="content-subtitle">Fitur:</h1>
 					<ul class="feature-list">
 						<li>Nama domain bebas (sesuai ketersediaan)</li>
 						<li>Gratis foto (maksimal 20 foto)</li>
@@ -89,10 +157,95 @@
 			</div>
 		</div>
 	
-		<div id="how-to" class="page image-page" style="background-color: #E65B5E; color: #2F2F3B;">
+		<div id="how-to" class="page content-page" style="background-color: #FF7066; color: #FFFFFF;">
+			<div class="page-content">
+				<h1 class="content-title">CIPTAKAN MILIKMU</h1>
+				<table>
+					<tr>
+						<td>
+							<i class="fa fa-lightbulb-o fa-5x"></i>
+							<span class="desc">Tentukan ide untuk websitemu</span>
+						</td>
+						<td>
+							<i class="fa fa-comment fa-5x"></i>
+							<span class="desc">Kirim dokumen atau kontak tim kami melalui chat box</span>							
+						</td>
+						<td>
+							<i class="fa fa-paint-brush fa-5x"></i>
+							<span class="desc">Diskusikan desain web dengan tim kami</span>
+						</td>
+						<td>
+							<i class="fa fa-camera fa-5x"></i>
+							<span class="desc">Jika perlu, tambahkan foto/gambar pendukung</span>
+						</td>
+						<td>
+							<i class="fa fa-globe fa-5x"></i>
+							<span class="desc">Tunggu dan websitemu akan siap kurang dari 24 jam</span>
+						</td>
+					</tr>
+				</table>
+			</div>
 		</div>
 	
-		<div id="about-us" class="page image-page" style="background-color: #2F2F3B; color: #E65B5E;">
+		<div id="about-us" class="page content-page" style="background-color: #2F2F3B; color: #FF7066;">
+			<div class="page-content">
+				<h1 class="content-title">TEAM MEMBER</h1>
+				<div class="box-4">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="box-4">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="box-4">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="box-4">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+			</div>
+		</div>
+		
+		<div id="contact-us" class="page content-page" style="background-color: #2F2F3B; color: #FFFFFF;">
+			<div class="page-content">
+				<h1 class="content-title">HUBUNGI KAMI</h1>
+				<div class="box-2">
+					<div class="item-with-icon">
+						<small class="item-label">TELEPON:</small>
+						<br/>
+						<h1 class="contact">08888888888888</h1>
+					</div>
+					<div class="item-with-icon">
+						<small class="item-label">EMAIL:</small>
+						<br/>
+						<h1 class="contact"><a href="mailto:info@tampilin.com">info@tampilin.com</a></h1>
+					</div>
+					<div class="item-with-icon">
+						<small class="item-label">FB:</small>
+						<br/>
+						<h1 class="contact"><a href="http://www.facebook.com/tampilin">Tampilin</a></h1>
+					</div>
+					<div class="item-with-icon">
+						<small class="item-label">IG:</small>
+						<br/>
+						<h1 class="contact"><a href="http://www.instagram.com/tampilin">@tampilin</a></h1>
+					</div>
+				</div>
+				<div class="box-2">
+					<input class="contact-input" type="text" placeholder="Nama" />
+					<input class="contact-input" type="text" placeholder="Email" />
+					<textarea rows="5" class="contact-input" placeholder="Isi Pesan"></textarea>
+					<button class="contact-button">Kirim Pesan</button>
+				</div>
+			</div>
+		</div>
+		
+		<div id="footer" class="page content-page" style="background-color: #ECECEC; color: #2F2F3B;">
+			<div class="page-content">
+				<p class="foot-note">
+				Copyright &copy; 2016 <a
+					href="https://www.facebook.com/tampilin" target="_blank">tampilin.com</a>
+				</p>
+			</div>
 		</div>
 	</div>
 	
