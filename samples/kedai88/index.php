b6237311b4d651e987a2ade4ab80b74a9e363627
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-02-21 00:27:56 +0700

    inisiasi baru buat kedai88. udah bisa fixed bg, keren banget lah :p

---
+++ samples/kedai88/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link
+	href='https://fonts.googleapis.com/css?family=Titillium+Web:400,600,700,300'
+	rel='stylesheet' type='text/css'>
+<title>Kedai 88: Chinese Cuisine from Yogyakarta</title>
+</head>
+<body>
+	<div class="header">
+		<ul>
+			<li><a href="#home">HOME</a></li>
+			<li><a href="#menu">MENU</a></li>
+			<li><a href="#contact">CONTACT US</a></li>
+			<li><a href="#about">ABOUT US</a></li>
+		</ul>
+	</div>
+	<div class="main">
+		<div class="home"></div>
+		<div class="about">
+			<div class="abouttext">
+				<h3 class="headertitle">ABOUT US</h3>
+				<h1 class="title">KEDAI 88</h1>
+				<h2 class="subtitle">
+					CHINESE CUISINE
+					</h1>
+					<p class="content">Kedai 88 Chinese Cuisine merupakan gerai
+						makan yang menggunakan resep asli turunan Tionghoa dengan
+						dipadukan dengan cita rasa masa kini. Kami hanya menggunakan bahan
+						yang segar dan bermutu tinggi.</p>
+			</div>
+			<div class="aboutimg"></div>
+		</div>
+		<div class="menu">
+			<div class="menuheader">
+				<h1 class="title">MENU</h1>
+			</div>
+			<div class="menucontent">
+				<div class="product">
+					<div class="product1"></div>
+					<div class="product2"></div>
+				</div>
+			</div>
+			<div class="toggle"></div>
+		</div>
+	</div>
+</body>
+</html>
