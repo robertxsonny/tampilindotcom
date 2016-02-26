3f88fa7787ebf0524bf9363ccd5968ea461bff9f
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-02-26 18:43:20 +0700

    inisiasi tempatnya rendy, udah bisa scroll header, main bg ditambah.

---
+++ samples/RendyRianS/index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" content="id">
+<meta name="description"
+	content="Tampilin.id, pengembang one page website sederhana di Indonesia. Menawarkan layanan yang cepat dan desain yang dibuat khusus, bukan berupa template. Buat Buat portofolio atau CV online, kartu nama online, profil usaha, atau website sederhana lainnya di sini.">
+<meta property="og:title"
+	content="Sample 1 for Tampilin.id: Chinese Cuisine">
+<meta property="og:description"
+	content="Tampilin.id, pengembang one page website sederhana di Indonesia. Menawarkan layanan yang cepat dan desain yang dibuat khusus, bukan berupa template. Buat Buat portofolio atau CV online, kartu nama online, profil usaha, atau website sederhana lainnya di sini.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://tampilin.id">
+<meta property="og:site_name" content="tampilin.id">
+<meta name="revisit-after" content="7 days">
+<meta name="distribution" content="web">
+<meta name="robots" content="index, follow">
+<link rel="shortcut icon" type="image/x-icon"
+	href="http://tampilin.id/images/icon.ico" />
+<link rel="shortcut icon" type="image/png" href="images/icon.png" />
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<link
+	href='https://fonts.googleapis.com/css?family=Titillium+Web:400,600,700,300'
+	rel='stylesheet' type='text/css'>
+<script src="../javascript/jquery-1.12.0.min.js" type="text/javascript"></script>
+<script src="../javascript/jquery-ui.js" type="text/javascript"></script>
+<script src="../javascript/index.js" type="text/javascript"></script>
+<title>About Me: Rendy Rian Sandhika</title>
+</head>
+<body>
+	<div class="header">
+		<div class="burger"></div>
+		<p class="phoneno">Rendy Rian Sandhika
+		</p>
+		<ul>
+			<li><a class="link" href="#about">ABOUT</a></li>
+			<li><a class="link" href="#menu">BLOG</a></li>
+			<li><a class="link" href="#gallery">GALLERY</a></li>
+			<li><a class="link" href="#works">WORK EXPERIENCES</a></li>
+			<li><a class="link" href="#education">EDUCATION</a></li>
+			<li><a class="link" href="#contact">CONTACT</a></li>
+		</ul>
+	</div>
+	<div class="main">
+		<div id="landingpage" class="home"></div>
+		<div id="about" class="about">
+			<div class="abouttext">
+				<h3 class="headertitle">ABOUT US</h3>
+				<h1 class="title">KEDAI 88</h1>
+				<h2 class="subtitle">
+					CHINESE CUISINE
+					</h1>
+					<p class="content">Kedai 88 Chinese Cuisine merupakan gerai makan
+						yang menggunakan resep asli turunan Tionghoa dengan dipadukan
+						dengan cita rasa masa kini. Kami hanya menggunakan bahan yang
+						segar dan bermutu tinggi.</p>
+			
+			</div>
+			<div class="aboutimg"></div>
+		</div>
+		<div id="menu" class="menu">
+			<div class="menuheader">
+				<h1 class="title">MENU</h1>
+			</div>
+			<div class="menucontent">
+				<div class="product">
+					<div class="product1"></div>
+					<div class="product2"></div>
+					<div class="product3"></div>
+				</div>
+			</div>
+			<div class="toggle">
+				<div class="prev">
+					<span></span>
+				</div>
+				<div class="next">
+					<span></span>
+				</div>
+				<div class="title">PAKET HEMAT</div>
+				<div class="subtitle">NASI + LAUK + SAYUR + MINUM</div>
+			</div>
+		</div>
+		<div id="menulist" class="menulist">
+			<div class="categories">
+				<div id="ayamtulangcat" class="category">
+					<div class="bgimg1"></div>
+					<div class="background selected"></div>
+					<div class="text">
+						<p>AYAM TULANG</p>
+					</div>
+				</div>
+				<div id="ayamfilletcat" class="category">
+					<div class="bgimg2"></div>
+					<div class="background"></div>
+					<div class="text">
+						<p>AYAM FILLET</p>
+					</div>
+				</div>
+				<div id="sapipotongcat" class="category">
+					<div class="bgimg3"></div>
+					<div class="background"></div>
+					<div class="text">
+						<p>SAPI POTONG</p>
+					</div>
+				</div>
+			</div>
+			<div class="categories-2-cols">
+				<div id="sapifilletcat" class="category">
+					<div class="bgimg4"></div>
+					<div class="background"></div>
+					<div class="text">
+						<p>SAPI FILLET</p>
+					</div>
+				</div>
+				<div id="bebekpotongcat" class="category">
+					<div class="bgimg5"></div>
+					<div class="background"></div>
+					<div class="text">
+						<p>BEBEK POTONG</p>
+					</div>
+				</div>
+			</div>
+			<div id="ayamtulang" class="list">
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM GORENG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM BAKAR</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM POTONG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM MADU</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM GULA</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM GURIH</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM PENYET</p>
+				</div>
+			</div>
+			<div id="ayamfillet" class="list">
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM NUGGET</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM GARAM</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM BLACKPAPPER PANGGANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>AYAM ABC</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+			</div>
+			<div id="sapipotong" class="list">
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+			</div>
+			<div id="sapifillet" class="list">
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+			</div>
+			<div id="bebekpotong" class="list">
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>PAKET HEMAT</p>
+				</div>
+				<div class="product">
+					<img src="/images/product1.jpg" />
+					<p>AYAM TULANG</p>
+				</div>
+				<div class="product">
+					<img src="/images/product2.jpg" />
+					<p>NASI GORENG</p>
+				</div>
+			</div>
+		</div>
+		<div id="contact" class="footer">
+			<div class="col">
+				<div class="logo"></div>
+				<p>Subha Food Fest</p>
+				<p>Galeria Mall Lt. 3</p>
+				<p>Jalan Jenderal Sudirman No. 99-101</p>
+				<p>Yogyakarta</p>
+				<p class="phoneno">
+					<span class="phone"></span>0813-2888-7771
+				</p>
+			</div>
+			<div class="col">
+				<h1>
+					<span class="location"></span>CONTACT US
+				</h1>
+				<iframe
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.0676275992882!2d110.37678031397499!3d-7.7826546793771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a58318198e939%3A0xb6283ff15225c290!2sGaleria+Mall!5e0!3m2!1sid!2sid!4v1456149885871"
+					width="550" height="350" frameborder="0" style="border: 0"
+					allowfullscreen></iframe>
+			</div>
+		</div>
+		<div class="copyright">
+			<p>
+				Copyright &#169; 2016 Kedai 88 by <a href="http://tampilin.id">Tampilin.id</a>:
+				One Stop Online Services
+			</p>
+			<small>Icons made by <a href="http://www.flaticon.com/authors/zurb">Zurb</a>
+				and <a href="http://www.flaticon.com/authors/freepik">Freepik</a>
+				from <a href="http://flaticon.com">www.flaticon.com</a></small>
+		</div>
+	</div>
+	<div class="popup">
+		<div class="overlay"></div>
+		<div class="close"></div>
+		<div class="fullscreen"></div>
+	</div>
+</body>
+</html>
---
+++ samples/RendyRianS/javascript/index.js
@@ -0,0 +1,242 @@
+var productindex = 1;
+
+$.fn.exists = function() {
+	return this.length !== 0;
+}
+
+$(document).ready(function() {
+	$('.popup').hide();
+	$('.list').hide();
+	$('#ayamtulang').slideToggle(500);
+	if ($(window).width() <= 768)
+		$('.header ul').hide();
+	$('.link').click(function() {
+
+		if ($(this).attr('href') == '#landingpage') {
+			$('html,body').animate({
+				scrollTop : 0
+			}, {
+				duration : 500
+			});
+			return false;
+		}
+
+		$('html,body').animate({
+			scrollTop : $($(this).attr('href')).offset().top
+		}, {
+			duration : 500
+		});
+		return false;
+	});
+
+	window.setInterval(function() {
+		productindex++;
+		$('.toggle').slideToggle(500);
+		if ($('.product' + productindex).exists()) {
+			window.setTimeout(function() {
+				setDescription();
+			}, 1000);
+
+			$('.product1').animate({
+				'marginLeft' : '-=1280'
+			}, 500)
+		} else {
+			productindex = 1;
+			window.setTimeout(function() {
+				setDescription();
+			}, 1000);
+			$('.product1').animate({
+				'marginLeft' : '0'
+			}, 500)
+		}
+
+		window.setTimeout(function() {
+			$('.toggle').slideToggle(500);
+		}, 1000);
+	}, 7000);
+
+	$('.toggle .prev').hover(function() {
+		$('.toggle .prev').find('span').addClass('hover');
+	}, function() {
+		$('.toggle .prev').find('span').removeClass('hover');
+	});
+
+	$('.toggle .next').hover(function() {
+		$('.toggle .next').find('span').addClass('hover');
+	}, function() {
+		$('.toggle .next').find('span').removeClass('hover');
+	});
+
+	$('.toggle .next').click(function() {
+		productindex++;
+		$('.toggle').slideToggle(500);
+		if ($('.product' + productindex).exists()) {
+			window.setTimeout(function() {
+				setDescription();
+			}, 1000);
+
+			$('.product1').animate({
+				'marginLeft' : '-=1280'
+			}, 500)
+		} else {
+			productindex = 1;
+			window.setTimeout(function() {
+				setDescription();
+			}, 1000);
+			$('.product1').animate({
+				'marginLeft' : '0'
+			}, 500)
+		}
+
+		window.setTimeout(function() {
+			$('.toggle').slideToggle(500);
+		}, 1000);
+	});
+
+	$('.toggle .prev').click(function() {
+		$('.toggle').slideToggle(500);
+		productindex--;
+		if ($('.product' + productindex).exists()) {
+			window.setTimeout(function() {
+				setDescription();
+			}, 1000);
+
+			$('.product1').animate({
+				'marginLeft' : '+=1280'
+			}, 500);
+		} else {
+			productindex++;
+			for (var i = 2; i <= 30; i++) { // max photo 30
+				if ($('.product' + i).exists()) {
+					productindex = i;
+					window.setTimeout(function() {
+						setDescription();
+					}, 1000);
+					$('.product1').animate({
+						'marginLeft' : '-=1280'
+					}, 500);
+				} else {
+					break;
+				}
+
+			}
+		}
+
+		window.setTimeout(function() {
+			$('.toggle').slideToggle(500);
+
+		}, 1000);
+	});
+
+	var lastscroll = 0;
+	$(document).scroll(function(e) {
+		//auto header 
+		
+		
+		if (lastscroll < $(document).scrollTop()) { // if scroll down
+			/*if ($(document).scrollTop() == 100) {
+				$('.home').animate({
+					'top' : '-=50'
+				}, 500);
+				$('.header').animate({
+					'top' : '0'
+				}, 300);
+			}*/
+			
+			if($(document).scrollTop() > 0 ){
+				$('.header').addClass('top-zero');
+			}
+			
+		} else { // if scroll up
+			/*if ($(document).scrollTop() == 300) {
+				$('.home').animate({
+					'top' : '0'
+				}, 250);
+				$('.header').animate({
+					'top' : '400'
+				}, 300);
+			} else if ($(document).scrollTop() == 0) {
+				$('.home').animate({
+					'top' : '0'
+				}, 250);
+				$('.header').animate({
+					'top' : '400'
+				}, 300);
+			}*/
+			if($(document).scrollTop() < 100){
+				$('.header').removeClass('top-zero');
+			}
+			
+		}
+		lastscroll = $(document).scrollTop();
+	});
+
+	$('.menulist .list .product img').click(function() {
+		$href = $(this).attr('src');
+		$('.popup').fadeIn('slow');
+		$('.popup .fullscreen').css('background-image', 'url("' + $href + '")')
+	});
+
+	$('.popup .close').click(function() {
+		$('.popup').fadeOut('slow');
+	});
+	$('.header .burger').click(function() {
+		$('.header ul').slideToggle(500);
+	});
+	$('.categories .category').hover(function() {
+		$(this).find('div.background').addClass('hover');
+	}, function() {
+		$(this).find('div.background').removeClass('hover');
+	});
+	$('.categories-2-cols .category').hover(function() {
+		$(this).find('div.background').addClass('hover');
+	}, function() {
+		$(this).find('div.background').removeClass('hover');
+	});
+	$('.categories .category').click(function() {
+		var id = "#" + $(this).attr('id').replace("cat", "");
+		$('.categories .category').find('div.background').removeClass('selected');
+		$('.categories-2-cols .category').find('div.background').removeClass('selected');
+		$(this).find('div.background').addClass('selected');
+		$('.list').hide('slide', {
+			direction : 'left'
+		}, 500);
+		window.setTimeout(function() {
+			$(id).show('slide', {
+				direction : 'right'
+			}, 500);
+		}, 500);
+	});
+	
+	$('.categories-2-cols .category').click(function() {
+		var id = "#" + $(this).attr('id').replace("cat", "");
+		$('.categories .category').find('div.background').removeClass('selected');
+		$('.categories-2-cols .category').find('div.background').removeClass('selected');
+		$(this).find('div.background').addClass('selected');
+		$('.list').hide('slide', {
+			direction : 'left'
+		}, 500);
+		window.setTimeout(function() {
+			$(id).show('slide', {
+				direction : 'right'
+			}, 500);
+		}, 500);
+	});
+});
+
+function setDescription() {
+	switch (productindex) {
+	case 1:
+		$('.toggle .title').html('PAKET HEMAT');
+		$('.toggle .subtitle').html('NASI + LAUK + SAYUR + MINUM');
+		break;
+	case 2:
+		$('.toggle .title').html('PAKET LENGKAP');
+		$('.toggle .subtitle').html('NASI + DAGING + LAUK + SAYUR + MINUM');
+		break;
+	case 3:
+		$('.toggle .title').html('PAKET IRIT');
+		$('.toggle .subtitle').html('NASI + MINUM');
+		break;
+	}
+}
