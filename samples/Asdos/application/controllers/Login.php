04b31f8755ded3ec67de35ad56df9ccbe84dd581
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-10-15 12:43:51 +0700

    update asdos API untuk profile picture dan last update profile

--- samples/Asdos/application/controllers/Login.php
+++ samples/Asdos/application/controllers/Login.php
@@ -1,5 +1,6 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+date_default_timezone_set('Asia/Jakarta');
 class Login extends CI_Controller {
 	public function __construct() {
 		parent::__construct ();
@@ -14,8 +15,8 @@ class Login extends CI_Controller {
 	public function index() {
 		$this->load->view ( 'welcome_message' );
 	}
-	public function get(){
-		echo json_encode($this->login_model->get_all_users());
+	public function get() {
+		echo json_encode ( $this->login_model->get_all_users () );
 		return;
 	}
     public function get_asdos(){
@@ -27,27 +28,35 @@ class Login extends CI_Controller {
 		return;
 	}
 	public function login() {
-		$this->trylogin($this->input->post ( 'username' ), md5($this->input->post ( 'password' )));
+		$this->trylogin ( $this->input->post ( 'username' ), md5 ( $this->input->post ( 'password' ) ) );
 	}
-	public function autologin(){
-		$this->trylogin($this->input->post ( 'username' ), $this->input->post ( 'password' ));
+	public function autologin() {
+		$this->trylogin ( $this->input->post ( 'username' ), $this->input->post ( 'password' ) );
 	}
-	public function register(){
-		$result = $this->login_model->register_user();
-		echo json_encode(array(
+	public function register() {
+		$result = $this->login_model->register_user ();
+		echo json_encode ( array (
 				'status' => '0',
-				'userid' => $result
-		));
+				'userid' => $result 
+		) );
 		return;
 	}
-	private function trylogin($username, $password){
+	public function profilepicture() {
+		echo $this->login_model->get_profile_picture ( $this->input->post('id'));
+		return;
+	}
+	public function updateprofilepicture() {
+		echo $this->login_model->update_profile_picture ( $this->input->post('id'), $this->input->post ( 'image' ));
+		return;
+	}
+	private function trylogin($username, $password) {
 		$result = $this->login_model->sign_in ( $username, $password );
 		if ($result != false) {
 			echo json_encode ( $result );
 			return;
 		} else {
 			echo json_encode ( array (
-					'status' => '1'
+					'status' => '1' 
 			) );
 			return;
 		}
--- samples/Asdos/application/models/Login_model.php
+++ samples/Asdos/application/models/Login_model.php
@@ -7,12 +7,61 @@ class Login_model extends CI_Model {
 		$this->db->order_by ( 'userid', 'asc' );
 		$query = $this->db->get ( 'users_with_types' );
 		$result = $query->result_array ();
-		if(count($result) > 0){
-			for ($i = 0; $i < count($result); $i++) {
-				unset($result[$i]['password']);
+		if (count ( $result ) > 0) {
+			for($i = 0; $i < count ( $result ); $i ++) {
+				unset ( $result [$i] ['password'] );
 			}
 		}
 		return $result;
+	}
+	public function update_profile_picture($id, $base64){
+		$this->db->where ( 'userid', $id );
+		$query = $this->db->get ( 'users_with_types' );
+		$result = $query->row_array ();
+		if($result != null){
+			$data = array(
+					'lastupdate' => date ( 'Y-m-d H:i:s' )
+			);
+			if ($base64 != '') { // if there is profpict
+				$fp = fopen ( $_SERVER ['DOCUMENT_ROOT'] . '/img/' . $id . '.txt', 'wb' );
+				fwrite ( $fp, $base64 );
+				fclose ( $fp );
+			}
+			$this->db->where ( 'id', $id );
+			$this->db->update ( 'users', $data );
+			
+			return json_encode( array (
+					'status' => '0'
+			) );
+		}
+		else{
+			return json_encode ( array (
+					'status' => '1'
+			) );
+		}
+		
+	}
+	public function get_profile_picture($id) {
+		$this->db->where ( 'userid', $id );
+		$query = $this->db->get ( 'users_with_types' );
+		$result = $query->row_array ();
+		if ($result != null) {
+			try {
+				if(!file_exists($_SERVER ['DOCUMENT_ROOT'] . '/img/' . $id . '.txt')){
+					throw new Exception('File: ' . '/img/' . $id . '.txt' . ' does not exist');
+				}
+				$fp = fopen ( $_SERVER ['DOCUMENT_ROOT'] . '/img/' . $id . '.txt', 'r' );
+				$result = array (
+						'id' => $id,
+						'base64' => fread ( $fp, filesize ( $_SERVER ['DOCUMENT_ROOT'] . '/img/' . $id . '.txt' ) ) 
+				);
+				return json_encode ( $result );
+			} catch ( Exception $e ) {
+				return json_encode ( array (
+						'status' => '1' 
+				) );
+			}
+		}
 	}
     public function get_all_asdos() {
         $this->db->where('typeid', 1);
@@ -42,16 +91,24 @@ class Login_model extends CI_Model {
 		$this->load->helper ( 'url' );
 		
 		$data = array (
-				'fullname' => $this->input->post('fullname'),
+				'fullname' => $this->input->post ( 'fullname' ),
 				'username' => $this->input->post ( 'username' ),
 				'password' => md5 ( $this->input->post ( 'password' ) ),
 				'institution' => $this->input->post ( 'institusi' ),
 				'field' => $this->input->post ( 'field' ),
-				'type' => $this->input->post ( 'type' ) 
+				'type' => $this->input->post ( 'type' ),
+				'lastupdate' => date ( 'Y-m-d H:i:s' ) 
 		);
 		$this->db->insert ( 'users', $data );
 		$insert_id = $this->db->insert_id ();
 		
+		// image
+		$base64 = $this->input->post ( 'image' );
+		if ($base64 != '') { // if there is profpict
+			$fp = fopen ( $_SERVER ['DOCUMENT_ROOT'] . '/img/' . $insert_id . '.txt', 'wb' );
+			fwrite ( $fp, $base64 );
+			fclose ( $fp );
+		}
 		return $insert_id;
 	}
 	public function sign_in($username, $md5) {
@@ -63,13 +120,14 @@ class Login_model extends CI_Model {
 			$userdata = array (
 					'status' => '0',
 					'userid' => $result ['userid'],
-					'fullname' => $result['fullname'],
+					'fullname' => $result ['fullname'],
 					'username' => $result ['username'],
 					'password' => $result ['password'],
 					'typeid' => $result ['typeid'],
 					'type' => $result ['type'],
 					'institution' => $result ['institution'],
-					'field' => $result ['field'] 
+					'field' => $result ['field'] ,
+					'lastupdate' => $result['lastupdate']
 			);
 			return $userdata;
 		} else
